3ae8abaabce0f390e927a7d0357bbef1dbc73ac9
António Figueiras <antonio.figueiras@tecnico.ulisboa.pt>
2025-11-20 21:51:16 +0000

    make available choice of gpus

--- main.py
+++ main.py
@@ -7,12 +7,20 @@ import os
 import pickle
 import torch
 import torch.multiprocessing as mp
+import numpy as np
 
-def main():
-    config=get_config()
-    run=init_wandb(project_name=config['project_name'], config_dict=config, run_name=config.get('run_name', None))
-    config.update(run.config)
-    print(config['learning_rate'])
+def main(config=None, run=None):
+
+    gpu_ids = config.get("gpu_ids", "all")  # e.g. [0,2,3]
+
+    if gpu_ids != "all":
+        os.environ["CUDA_VISIBLE_DEVICES"] = ",".join(map(str, gpu_ids))
+
+    SEED = 42
+    np.random.seed(SEED)
+    torch.manual_seed(SEED)
+    torch.cuda.manual_seed_all(SEED)
+
     processed_file=f"{config['species']}{config['k_mers']}{config['fam_type']}.pt"
 
     os.makedirs("data/processed", exist_ok=True)
@@ -30,10 +38,34 @@ def main():
     dataset.train_mask = mask[0]
     dataset.test_mask = mask[1]
 
-    world_size = torch.cuda.device_count()
+    if torch.cuda.is_available():
+        if gpu_ids == "all":
+            world_size = torch.cuda.device_count()
+        else:
+            world_size = len(gpu_ids)
+        print(f"Using {world_size} GPUs for training")
+    else:
+        world_size = 1
+        print("CUDA not available, using CPU for training")
+
+    print("Starting training...")
     mp.spawn(train, args=(world_size, dataset, config, run), nprocs=world_size, join=True)
 
-    finish_wandb(run)
-
 if __name__ == "__main__":
-    main()
+    config=get_config()
+
+    run=init_wandb(
+        project_name=config["project_name"],
+        config_dict=config,
+        run_name=config.get("run_name", None)
+    )
+    config.update(run.config)
+    print(config['learning_rate'])
+    try:
+        main(config, run)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        run.log({"error": str(e)})
+        raise e
+    finally:
+        finish_wandb(run)
